6329f68f7cd9994e5a57c9ce6e4807259446d042
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-01-27 01:16:08 +0000

    開発用postgres追加

--- app/src/crawlers/qiita/handler.ts
+++ app/src/crawlers/qiita/handler.ts
@@ -8,7 +8,7 @@ requestHandler.addHandler('ARTICLE', async ({ request, page, log }) => {
     const content = await page.locator(".style-itrjxe").first().textContent();
     const hash = createHash('md5').update(content || '').digest('hex');
     const url = request.url;
-    log.info(`title is ${title},url: ${url}, content-hash: ${hash}`);
+    log.info(`title is ${title},url: ${url},content-hash: ${hash}`);
 });
 
 requestHandler.addDefaultHandler(async ({ request, page, enqueueLinks, log }) => {
@@ -16,7 +16,6 @@ requestHandler.addDefaultHandler(async ({ request, page, enqueueLinks, log }) =>
     // Extract links from the current page
     // and add them to the crawling queue.
     const result = await enqueueLinks({ selector, label: "ARTICLE" });
-    log.debug("debug: ", result);
     if (0 < result.unprocessedRequests.length) {
         // 指定したセレクタでデータが取得できない場合はhtml構造が変化した可能性があるので通知処理を行う
     }
