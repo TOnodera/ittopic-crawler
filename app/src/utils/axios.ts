e7d2f41fb02392148d51cfbad1a4888a272b2cf4
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-02-06 07:14:48 +0900

    作りおかしいところ修正

--- app/src/utils/axios.ts
+++ app/src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 
 const http = axios.create({
-  baseURL: `http://${process.env.PRIVATE_API_DOMAIN_NAME}:${process.env.API_SERVICE_PORT}/api-private/`,
+  baseURL: `http://${process.env.PRIVATE_API_DOMAIN_NAME}/api-private/`,
 });
 
 axiosRetry(axios, { retries: HTTP_RETRY_COUNT });
