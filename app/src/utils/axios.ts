c09ca4eee9351979c7c3786d8af879dcb2ad37ec
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-02-06 13:39:51 +0000

    keepalive設定を追加

--- app/src/utils/axios.ts
+++ app/src/utils/axios.ts
@@ -1,9 +1,16 @@
 import { HTTP_RETRY_COUNT } from '@/config.js';
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
+import { Agent } from 'http';
 
+const baseURL = ['development', 'test'].includes(process.env.NODE_ENV as string)
+  ? `http://${process.env.PRIVATE_API_DOMAIN_NAME}:3000/api-private/`
+  : `http://${process.env.PRIVATE_API_DOMAIN_NAME}/api-private/`;
+console.debug('baseURL: ', baseURL);
+console.debug('NODE_ENV:', process.env.NODE_ENV);
 const http = axios.create({
-  baseURL: `http://${process.env.PRIVATE_API_DOMAIN_NAME}/api-private/`,
+  baseURL,
+  httpAgent: new Agent({keepAlive: false})
 });
 
 axiosRetry(axios, { retries: HTTP_RETRY_COUNT });
