011a865735e7cd2870714cf6f856ced0af37c52e
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-02-07 20:52:17 +0000

    いらないテスト削除

--- app/tests/integration/ArticleDomain.ts
+++ app/tests/integration/ArticleDomain.ts
@@ -1,12 +1,8 @@
 import { test, describe, expect } from '@jest/globals';
 import { ArticleDomain } from '@/domain/ArticleDomain.js';
 import { AppStore, NewArticle } from '@/store/AppStore.js';
-import { PrismaClient } from '@prisma/client';
 import { SITE } from '@/config.js';
-import { resetAndSeedDatabase, resetDatabase } from '../settings/utility.js';
 
-const client = new PrismaClient();
-const store = new AppStore(client);
 const article = {
   title: 'title',
   content: 'content',
@@ -14,33 +10,8 @@ const article = {
   contentId: 'contentId',
   url: 'https://example.com',
   siteId: SITE.QIITA,
+  ogpTitle: 'ogpTitle',
+  ogpDescription: 'ogpDesc',
+  ogpImage: 'ogpImage',
 } as NewArticle;
-describe('ArticleDomainのテスト', () => {
-  beforeAll(async () => {
-    return await resetAndSeedDatabase(client);
-  });
-  test('タイトルがない場合はfalse', async () => {
-    const domain = new ArticleDomain(store, SITE.QIITA);
-    const result = await domain.save({ ...article, title: ' ' });
-    expect(result).toBeFalsy();
-  });
-  test('本文がない場合はfalse', async () => {
-    const domain = new ArticleDomain(store, SITE.QIITA);
-    const result = await domain.save({ ...article, content: ' ' });
-    expect(result).toBeFalsy();
-  });
-  test('既存データがない場合は新規登録', async () => {
-    const domain = new ArticleDomain(store, SITE.QIITA);
-    const result = await domain.save(article);
-    expect(result).toBeTruthy();
-  });
-  test('既存データがある場合は更新', async () => {
-    const domain = new ArticleDomain(store, SITE.QIITA);
-    const updatedContent = 'content2';
-    const updatingArticle = { ...article, content: updatedContent };
-    const updatedArticle = await domain.save(updatingArticle);
-    expect(updatedArticle).toBeTruthy();
-    const oldArticle = await store.getArticle(1);
-    expect(oldArticle?.content).toBe(updatedContent);
-  });
-});
+describe('ArticleDomainのテスト', () => {});
--- app/tests/unit/store/crawlerStatsStore.ts
+++
@@ -1,3 +0,0 @@
-import { test, describe, expect } from '@jest/globals';
-import { PrismaClient } from '@prisma/client';
-import { resetAndSeedDatabase } from '../../settings/utility.js';
